300e4b2846c8a60920619d03e6d71211b9cf3fef
hintonandrew510 <hintonandrew510@gmail.com>
2020-03-12 20:57:06 -0400

    Update HelloWorld.java

--- src/java/org/HelloWorld.java
+++ src/java/org/HelloWorld.java
@@ -2,7 +2,7 @@
  *  Copyright (c) Red Hat, Inc. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-package org.eclipse.che.examples;
+package org;
 
 public class HelloWorld {
     public static void main(String... argvs) {
