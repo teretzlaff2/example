e7ca5fda7460ba725903a0b8daac67ce01f1af12
teretzlaff2 <62122357+teretzlaff2@users.noreply.github.com>
2020-03-26 20:22:02 -0400

    Update AccountTest.java

--- src/main/java/org/AccountTest.java
+++ src/main/java/org/AccountTest.java
@@ -1,24 +1,30 @@
    package org;
    
-   import java.util.Scanner;
+   //import java.util.Scanner;
 
    public class AccountTest {
-      public static void main(String[] args) {
-         
-         Scanner input = new Scanner(System.in);
-         
-         Account myAccount = new Account();
-         
-         System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+      public static void main(String[] args) {         
 
-         System.out.println("Please enter the name:");
-         String theName = input.nextLine(); 
-         myAccount.setName(theName); 
-         System.out.println(); 
-         
-         System.out.printf("Name in object myAccount is:%n%s%n",
-            myAccount.getName());
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
 
-            input.close();
+        System.out.printf("account1 name is: %s%n", account1.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
+
+        //  Scanner input = new Scanner(System.in);
+         
+        //  Account myAccount = new Account();
+         
+        //  System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+
+        //  System.out.println("Please enter the name:");
+        //  String theName = input.nextLine(); 
+        //  myAccount.setName(theName); 
+        //  System.out.println(); 
+         
+        //  System.out.printf("Name in object myAccount is:%n%s%n",
+        //     myAccount.getName());
+
+        //     input.close();
       }
    }
